e5d3f57037602682250a08376eb4e7b1f4413160
Benjamin Stokes <stokesbnj@gmail.com>
2015-07-15 18:41:20 +0100

    Adds flightTracker() to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,32 @@
 function helloWorld() {
     document.getElementById("hello").innerHTML="Hello, World!";
 }
+
+function toggleVisibility(id) {
+    var e = document.getElementById(id);
+    if (e.style.display == 'none' || e.style.display=='')
+    {
+        e.style.display = 'block';
+    }
+    else 
+    {
+        e.style.display = 'none';
+    }
+}
+
+function flightTracker() {
+    var today, day, month;    
+    today = new Date();
+    day = today.getDate();
+    month = today.getMonth() + 1;
+        
+    if (day == 18 && month == 7) {
+        document.getElementById("flightCheck").innerHTML="Track the outbound flight here: LINK";
+    }
+    else if ((day == 31 && month == 7) || ( day == 01 && month == 08)) {
+        document.getElementById("flightCheck").innerHTML="Track the return flight here: LINK";
+    }
+    else {
+        document.getElementById("flightCheck").innerHTML="";
+    }
+}
