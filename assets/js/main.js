bdcba304770b149a97fe6ce8d4911814e6087f81
Benjamin Stokes <stokesbnj@gmail.com>
2015-09-02 18:48:41 +0100

    Simplifies date comparrison structure in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,82 +72,59 @@ function tripStatus() {
     time = hour + ':' + mins;
         
     var div = document.getElementById("tripStatus");
-    var hu = "<b>Holiday Update:</b> "
+    var hu = "<b>Holiday Update:</b> ";
     var intro = "We are currently ";
     
-    // Flight to the USA - 
-    if (theDate == '18-7-2015') {
-        if (time < '12:05') {
-            div.innerHTML= hu + "We're flying today at 12:05 (UK)!";
-        }
-        else if ((time >= '12:05') && (time < '20:15')) {
-            div.innerHTML= hu + intro + "flying from London to Washington Dulles!<br><i class=\"fa fa-plane\"></i><a href=\"https://www.google.co.uk/search?q=UA919\"> Track the flight!</a>"; //UA919
-        }
-        else if (time > '23:53') {
-            div.innerHTML= hu + "We're in St Louis!";
-        }
-        else {
-            div.innerHTML= hu + intro + "flying between Washington Dulles & St Louis!<br><i class=\"fa fa-plane\"></i><a href=\"https://www.google.co.uk/search?q=UA3434\"> Track the flight!</a>"; //UA3434
-        }
+    
+    // Section 1 - Pre-Holiday
+    var departureDay = new Date('2015-7-18');
+    
+    // Section 2 - Flight from London to St. Louis
+    var londonToSTLDepart = new Date('2015-7-18 12:05:00');
+    var londonToSTLArrival = new Date('2015-7-18 23:53:00');
+    
+    // Section 3 - Flight from St. Louis to Austin & time in Austin
+    var stlToAustin = new Date('2015-7-19 18:10:00');
+    
+    // Section 4 - Flight from Austin to St. Louis & time in St. Louis
+    var austinToSTL = new Date('2015-7-26');
+    
+    // Section 5 - Flight from St. Louis to London
+    var stlToChicago = new Date();
+    var chicagoToLondon = new Date();
+    
+    
+    
+    // Section 1 - Pre-Holiday
+    if (today < departureDay) {
+        div.innerHTML= hu + "We're flying on the 18th July at 12:05 (GMT)!";
     }
-    // Flight from STL --> AUSTIN - WN3054 (South West)
-    else if (theDate == '19-7-2015'){
-        if (time < '21:15') {
-            div.innerHTML= hu +  intro + "in St Louis but our flight to Austin leaves at 21:15 (UK)";
-        }
-        else if (time > '23:10') {
-            div.innerHTML= hu + "We're in Austin!";
-        }
-        else {
-            div.innerHTML= hu +  intro + "flying between St Louis & Austin!<br><i class=\"fa fa-plane\"></i><a href=\"https://www.google.co.uk/search?q=WN3054\"> Track the flight!</a>";
-        }
-    }
-    // Our stay in Austin - 19th - 26th
-    else if ((theDate > '19-7-2015') && (theDate < '26-7-2015')) {
-        div.innerHTML= hu + "We're in Austin!";
-    }
-    // Our stay in STL
-    else if ((theDate > '26-7-2015') && (theDate < '1-8-2015')) {
-         div.innerHTML= hu + "We're in St. Louis!";
+    else if (today < londonToWashington) {
+        div.innerHTML= hu + "We're flying today at 12:05 (UK)!";
     }
     
-    // Flight from AUSTIN --> STL - WN2876 (South West)
-    // 26th 19:25. 22:25 landing. UK time
-    else if (theDate == '26-7-2015') {
-        if (time < '19:25') {
-            div.innerHTML= hu + "We're in Austin!";
-        }
-        else if (time > '22:15') {
-            div.innerHTML= hu + "We're in St Louis!";
-        }
-        else {
-            div.innerHTML= hu +  intro + "flying between Austin & St Louis!<br><i class=\"fa fa-plane\"></i><a href=\"https://www.google.co.uk/search?q=WN2876\"> Track the flight!</a>";
-        }
+    // Section 2 - Flight from London to St. Louis
+    else if ((today >= londonToSTLDepart) && (today >= londonToSTLArrival)) {
+        div.innerHTML= hu + "flying between London & St Louis!";
     }
     
-    // Flight home
-    //  STL (00:35 1st) --> ORD (01:57 1st)
-    // ORD (03:05 1st) --> LHR (11:20 1st)
-    else if (theDate == '1-8-2015') {
-        if (time < '00:35') {
-            div.innerHTML= hu + "We're flying today at 00:35 (UK)!";
-        }
-        else if ((time >= '00:35') && (time > '01:57')) {
-             intro + "flying from St Louis to Chicago!<br><i class=\"fa fa-plane\"></i><a href=\"https://www.google.co.uk/search?q=UA3705\"> Track the flight!</a>"; //UA3705
-        }
-        else if (time > '01:57') {
-            div.innerHTML= hu + "We're in Chicago!";
-        }
-        else {
-            div.innerHTML= hu +  intro + "flying between Chicago & London!<br><i class=\"fa fa-plane\"></i><a href=\"https://www.google.co.uk/search?q=UA938\"> Track the flight!</a>" //UA938
-        }
-     }
-     
-     else if (theDate > '1-8-2015') {
-         div.innerHTML= hu + "The holiday is over and we're back at <i class=\"fa fa-home\"></i>!";
-     }
+    // Section 3.1 - Flight from St. Louis to Austin
     
+    
+    // Section 3.2 - Time in Austin
+   
+
+    // Section 4.1 - Flight from Austin to St. Louis
+    
+    
+    // Section 4.2 - Time in St. Louis
+
+
+    // Section 5 - Flight from St. Louis to London
+    
+    
+    // Section 6 - Post Holiday
     else {
-        div.innerHTML= hu + "We're in London! <i class=\"fa fa-home\"></i>";
-    }
+        div.innerHTML= hu + "The holiday is over and we're back at <i class=\"fa fa-home\"></i>!";
+    }   
 }
